ec997d5afcec071abce514e3fd2d1d5de1eaaebb
Alan Gooding <alangooding90@gmail.com>
2023-04-24 09:21:03 -0400

    changes

--- static/script.js
+++ static/script.js
@@ -336,3 +336,27 @@ function loadBlogDetails() {
     }
   }
 }
+
+        function searchDevice() {
+            var input, filter, table, tr, td, i, txtValue;
+            input = document.getElementById("searchDevice");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("cookbook-items");
+            tr = table.getElementsByTagName("tr");
+            for (i = 0; i < tr.length; i++) {
+                td1 = tr[i].getElementsByTagName("td")[2];
+                td2 = tr[i].getElementsByTagName("td")[3];
+                td3 = tr[i].getElementsByTagName("td")[4];
+                console.log(td)
+                if (td1 || td2 || td3) {
+                    txtValue1 = td1.textContent || td1.innerText;
+                    txtValue2 = td2.textContent || td2.innerText;
+                    txtValue3 = td3.textContent || td3.innerText;
+                    if (txtValue1.toUpperCase().indexOf(filter) > -1 || txtValue2.toUpperCase().indexOf(filter) > -1 || txtValue3.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
